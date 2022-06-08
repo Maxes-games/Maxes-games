d11ac845bff233e086aeb7c3de406cadc7ed1652
Maxes-games <107151424+Maxes-games@users.noreply.github.com>
2022-06-08 22:46:53 +0200

    Add files via upload

---
+++ Loginscreen/screenwidth.js
@@ -0,0 +1,19 @@
+setInterval(GetWidth,100);
+function GetWidth() {
+    var width = window.innerWidth;
+    if (width > 580){
+        document.getElementById('Title').style.fontSize=width / 25+"px";
+        document.getElementById('NoAcc').style.fontSize=width / 60+"px";
+        document.getElementById('Signup').style.fontSize=width / 60+"px";
+        document.getElementById('Username').style.fontSize=width / 60+"px";
+        document.getElementById('Password').style.fontSize=width / 60+"px";
+        document.getElementById('Submit').style.fontSize=width / 60+"px"; 
+    } else {
+        document.getElementById('Title').style.fontSize=width / 18+"px";
+        document.getElementById('NoAcc').style.fontSize=width / 36+"px";
+        document.getElementById('Signup').style.fontSize=width / 36+"px";
+        document.getElementById('Username').style.fontSize=width / 36+"px";
+        document.getElementById('Password').style.fontSize=width / 36+"px";
+        document.getElementById('Submit').style.fontSize=width / 36+"px"; 
+    }
+}
---
+++ Loginscreen/submit.js
@@ -0,0 +1,6 @@
+function Submit(){
+    let Username = document.getElementById("Username").value
+    let Password = document.getElementById("Password").value
+    let rePassword = document.getElementById("rePassword").value
+    console.log(Username + Password + rePassword);
+}
